fb9e153581482414240b7cf9cd53e799a0d18585
Tero Laitinen <tero.laitinen@feriko.fi>
2015-01-08 15:06:36 +0200

    constraining number of selected pieces, tracking already present and to-avoid selected music pieces

--- MusicSelect/Solve.hs
+++ MusicSelect/Solve.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
 module MusicSelect.Solve where
 
 import MusicSelect.Types
@@ -6,6 +9,10 @@ import Data.LinearProgram
 import Control.Monad.LPMonad
 import Control.Monad
 import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Control.Monad.State
+
+import qualified Data.List as L
 
 
 mpVar :: MusicPieceId -> String
@@ -17,15 +24,29 @@ inGenreVar mgId = "in" ++ show mgId
 missingVar :: MusicGenreId -> String
 missingVar mgId = "missing" ++ show mgId
 
+selectedVar :: String
+selectedVar = "selected"
+
+currentVar :: String
+currentVar = "current"
+
 avoidVar :: String
 avoidVar = "avoid"
 
-objFun :: MusicCounts -> LinFunc String Double
+
+
+objFun :: MusicCounts -> LinFunc String Int
 objFun counts = do
     
     linCombination $ [ 
             (-1, avoidVar)
         ]
+
+setBounds :: forall (m :: * -> *) v c.
+            (Ord c, Ord v, MonadState (LP v c) m) =>
+            v -> c -> c -> m ()
+setBounds v l u    | l == u = varEq v l
+    | otherwise = varBds v l u
 
 solve :: Requirements -> IO Result 
 solve reqs = do
@@ -50,12 +71,36 @@ solve reqs = do
         lp = execLPM $ do
             setDirection Max
             setObjective (objFun musicCounts)
+            -- music piece can be selected=1 or not=0
+            forM_ allMusic $ \mpId -> do
+                setVarKind (mpVar mpId) IntVar
+                setBounds (mpVar mpId) 0 1
+            -- number of selected music pieces must be 
+            -- less than the sum of channel music counts
+            setVarKind selectedVar IntVar
+            equal (var selectedVar) $ varSum $ map mpVar allMusic
+            setBounds selectedVar 0 totalCount
+
+            -- number of selected music pieces that are already present
+            setVarKind currentVar IntVar
+            equal (var currentVar) $ varSum $ [ mpVar mpId | mpId <- allMusic,
+                mpId `Set.member` current ]
+
+            -- total number of selected music pieces to avoid
+            setVarKind avoidVar IntVar
+            equal (var avoidVar) $ varSum $ map mpVar $ reqAvoidMusic reqs
+            -- count the number of selected music pieces in each
+            -- music genre
             forM_ (reqMusicGenres reqs) $ \mg -> do
+                setVarKind (inGenreVar $ mgId mg) IntVar
                 equal (var (inGenreVar $ mgId mg)) $ 
-                    varSum [ mpVar mpId | mpId <- mgPieces mg ]
-                                            
-                
+                    varSum [ mpVar mpId | mpId <- mgPieces mg,
+                             mpId `Set.notMember` banned ]
+        totalCount = sum $ map chMusicCount $ reqChannels reqs
+        allMusic = map head $ L.group $ L.sort $ concatMap mgPieces $ reqMusicGenres reqs        
         (startDate, endDate) = reqPeriod reqs
+        banned = Set.fromList $ reqBannedMusic reqs
+        current = Set.fromList $ reqCurrentMusic reqs
         musicCounts = Map.unionsWith max channelMusicCounts
         channelMusicCounts = [
                 musicFormatToMusicCount (chMusicCount ch) $ 
